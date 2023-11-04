5def0ee2f57f861d78e7251c66cbd4071f1bb121
MohammadEhsan Akhavanpour <akhavanm@uwindsor.ca>
2023-11-04 12:59:33 -0400

    added new line and removed unused import

--- backend/src/main/java/com/ehsan/Main.java
+++ backend/src/main/java/com/ehsan/Main.java
@@ -16,8 +16,8 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext configurableApplicationContext =
-                SpringApplication.run(Main.class, args);
+        SpringApplication.run(Main.class, args);
+
     }
 
 //    @Bean
