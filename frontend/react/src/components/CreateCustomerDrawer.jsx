b0e2a2577b13d0fcf3e966ac867218fb7b03f607
MohammadEhsan Akhavanpour <akhavanm@uwindsor.ca>
2023-11-20 23:42:39 -0500

    Updating .dockerignore

--- frontend/react/src/components/CreateCustomerDrawer.jsx
+++ frontend/react/src/components/CreateCustomerDrawer.jsx
@@ -15,7 +15,7 @@ const CreateCustomerDrawer = ({ fetchCustomers }) => {
     const {isOpen, onOpen, onClose} = useDisclosure()
     return <>
         <Button colorScheme={"teal"} leftIcon={<AddIcon/>} onClick={onOpen} >
-            Creating A New Customer
+            Creating New Customer
         </Button>
         <Drawer isOpen={isOpen} onClose={onClose} size={"lg"}>
             <DrawerOverlay/>
