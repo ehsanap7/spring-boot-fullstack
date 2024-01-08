ceb8ecd0181dc1ca2a21a3fd581fa3731cc120bd
MohammadEhsan Akhavanpour <akhavanm@uwindsor.ca>
2024-01-08 09:48:59 -0500

    Add CORS support to Spring Security and update FilterChain configuration.

--- backend/src/main/java/com/ehsan/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/ehsan/security/SecurityFilterChainConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -31,6 +32,7 @@ public class SecurityFilterChainConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/api/v1/insert")
                         .permitAll()
